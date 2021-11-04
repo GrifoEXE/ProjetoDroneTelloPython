7a996bd97463f0266f517527dce083693a70b2d8
Pedro Figueiredo <81266643+SpideyGriff@users.noreply.github.com>
2021-11-04 08:16:09 -0300

    Basic Movements

---
+++ main.py
@@ -0,0 +1,15 @@
+from djitellopy import Tello, tello
+
+from time import sleep
+
+me = tello.Tello()
+me.connect()
+
+print(me.get_battery())
+
+me.takeoff()
+
+me.send_rc_control(0,0,0,0)
+sleep(2)
+
+me.land()
